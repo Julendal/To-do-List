f4d47eb8dc6fabd4777e5e02ba1441ebc915f72d
Simon Julendal <simonjulendal1201@gmail.com>
2024-06-09 12:56:08 +0200

    Output in console

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const input_frm = document.querySelector(".input-container form");
+
+input_frm.addEventListener("submit", (e) => {
+    event.preventDefault()
+    let user_input = document.querySelector("form input");
+    console.log(user_input.value);
+    user_input.value = "";
+});
