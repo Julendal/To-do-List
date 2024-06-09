51f83d4fd976e31efb6d8dbc999d623b03f2bf56
Simon Julendal <simonjulendal1201@gmail.com>
2024-06-09 13:43:40 +0200

    Almost done, missing delete function

--- script.js
+++ script.js
@@ -2,7 +2,33 @@ const input_frm = document.querySelector(".input-container form");
 
 input_frm.addEventListener("submit", (e) => {
     event.preventDefault()
+    
+    // Create element
+    function create_element(html) {
+        const template = document.createElement("template");
+        template.innerHTML = html.trim();
+        return template.content.firstElementChild;
+    };
+
+    
+
+    // Ceate element with user input
     let user_input = document.querySelector("form input");
-    console.log(user_input.value);
+    
+    const user_output = create_element(`
+        <div class="output">
+            <p>${user_input.value}</p>
+            <button class="delete-btn">Delete</button>
+        </div>
+    `);
+
+    
+
+    // Inject to DOM
+    const output_container = document.querySelector(".output-container");
+    
+    output_container.appendChild(user_output);
+    
+    // Reset input field
     user_input.value = "";
 });
